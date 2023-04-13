8e7a0d20080dc635263052cb0720f633106fa4d1
martapla <martaplanella@hotmail.com>
2023-04-13 16:46:33 +0200

    props css

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <body>
-    <MyPlant :plantes="plantes" title="🌿  PLANTS  🌿"/>
+    
+    <MyPlant  :nameprops="plantList[0]" title="🌿  PLANTS  🌿"/>
+    <MyPlant  :nameprops="plantList[1]" />
+    <MyPlant  :nameprops="plantList[2]" />
+
+    
+
   </body>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import MyPlant from './MyPlant.vue';
 
+const plantList = ref([
+  {
+    name: 'Fern',
+    description: ' The fern is a type of plant that has fronds instead of leaves, and reproduces through spores instead of seeds. It is a common sight in gardens and forests, and its delicate appearance belies its hardiness and ability to thrive in diverse environments.',
+    image: 'https://images.unsplash.com/photo-1599416024757-71334210456b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZmVybnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    name: 'Cactus',
+    description: 'A cactus is a type of succulent plant that is adapted to hot and dry environments. Its characteristic thick and fleshy stem is covered in spines, which help to deter predators and reduce water loss. Despite its prickly exterior, many species of cactus produce beautiful flowers and fruits, and some even have medicinal properties.',
+    image: 'https://images.unsplash.com/photo-1554631221-f9603e6808be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FjdHVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    name: 'Monstera',
+    description: 'The monstera deliciosa, also known as the Swiss cheese plant, is a tropical plant with large, glossy leaves. It requires bright, indirect light and regular watering.',
+    image: 'https://images.unsplash.com/photo-1590940485457-413fe8caed47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vbnN0ZXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  }
+]);
 </script>
 
-<style scoped>
+<style >
+*{
+  /* border: 1px solid green; */
+}
 
 body {
   display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: center;
-  height: 50%;
+  height: 100%;
   width: 100%;
   margin: 0 auto;
   background-color: rgb(244, 201, 186);
--- src/MyPlant.vue
+++ src/MyPlant.vue
@@ -1,75 +1,88 @@
 <template>
-   
-    <h1>
-      {{title}}
-    </h1>
-    <div class="container">  
-     <ul>
-      <li v-for="plant in plantes" :key="plant.name">
-        <h2>{{ plant.name }}</h2>
-        <img :src="plant.image" :alt="plant.name" />
+  <h1>
+    {{ title }}
+  </h1>
+  <div class="container">
+    <div class="box-name">
+      <h2 class="plant-name"> {{ props.nameprops.name }}</h2>
+    </div>
+    <div class="box-img-desc">
+      <img :src="props.nameprops.image" :alt="props.nameprops.name" />
+      <span class="description">{{ props.nameprops.description }}</span>
 
-      </li>
-    </ul>
+    </div>
   </div>
-
 </template>
 
 <script setup>
-// import { ref, reactive } from 'vue';
 
-  defineProps({
-    title: {
-      type:String,
-      required: false,
-      default: " 🐠 ",
-    },
-  });
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: " 🐠 ",
+  },
+  nameprops: {
+    type: Object,
+    required: false,
+    default: "noname",
+  },
 
-  const plantes = [
-  {
-    name: 'Fern',
-    image: 'https://images.unsplash.com/photo-1599416024757-71334210456b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZmVybnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
-  },
-  {
-    name: 'Cactus',
-    image: 'https://images.unsplash.com/photo-1554631221-f9603e6808be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FjdHVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-  },
-  {
-    name: 'Monstera',
-    image: 'https://images.unsplash.com/photo-1590940485457-413fe8caed47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vbnN0ZXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-  }
-]
-  
+});
+
 </script>
 
-<style scoped>
+<style >
+* {
+  /* border: 1px solid red; */
+}
+
 h1 {
   color: rgb(38, 210, 135);
   margin: 100px;
   font-size: 80px;
 }
-h2{
-  color:green;
+
+h2 {
+  color: green;
+  font-size: 30px;
 }
+
 .container {
   display: flex;
   flex-direction: row;
   justify-content: center;
+  align-items: center;
+  height: 400px;
+  width: 80%;
+  margin: 0 auto;
+  border: 1px solid rgb(229, 219, 229);
+
 }
 
-ul li {
+.box-name {
+  border: 1px solid rgb(255, 225, 0);
+}
+
+.box-img-desc {
+  border: 1px solid rgb(235, 71, 224);
   display: flex;
   flex-direction: row;
   justify-content: center;
-  list-style: none;
-  margin-bottom: 100px;
-  width:100%;
-}
-img {
-  width: 60%;
-  border-radius: 50%;
-  border:4px solid greenyellow;
+  align-items: center;
+  padding:20px;
+  height: 300px;
+  width:80%;
 }
 
+img {
+  width: 60%;
+  height: 60%;
+  border-radius: 50%;
+  border: 4px solid greenyellow;
+}
+.description {
+  width: 50%;
+  border: 1px solid rgb(38, 37, 38);
+}
 </style>
